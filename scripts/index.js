b3e0b5740db59f36d95cd26830cab067122d8da8
Enzo <enzocass@live.com>
2022-07-01 19:14:45 -0300

    feat(index.html): Added page and card design

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const EVENTS = document.querySelector(".events");
+const EVENTS = document.querySelector(".events__cards");
 
 const BASE_URL = "http://localhost:3000";
 
@@ -24,19 +24,53 @@ async function renderEvents() {
             place } = event
     
         const eventDiv = document.createElement('div');
+        const imageDiv = document.createElement('div');
+        const titleDiv = document.createElement('div');
+        const artistsDiv = document.createElement('div');
+        const placeDiv = document.createElement('div');
+        const dateDiv = document.createElement('div');
+        const priceDiv = document.createElement('div');
+
+
         const eventImage = document.createElement('img');
         const eventTitle = document.createElement('h3');
         const eventArtists = document.createElement('p');
         const eventPlace = document.createElement('p');
+        const eventDate = document.createElement('p');
         const eventPriceStart = document.createElement('p');
     
-        //Estilos
-    
-        //Valores
-        eventImage.src = image
+        eventDiv.classList.add('event__card');
+        imageDiv.classList.add('card__image');
+        titleDiv.classList.add('card__title');
+        artistsDiv.classList.add('card__artist');
+        dateDiv.classList.add('card__date');
+        placeDiv.classList.add('card__place');
+        priceDiv.classList.add('card__price');
 
-        // atribuir na event div
-        eventDiv.appendChild(eventImage);
+        eventImage.classList.add('image__img');
+        eventTitle.classList.add('title__text');
+        eventPlace.classList.add('place__text')
+
+        eventImage.src = image;
+        eventTitle.innerHTML = title;
+        eventArtists.innerHTML = artists;
+        eventDate.innerHTML = date;
+        eventPlace.innerHTML = place;
+        eventPriceStart.innerHTML = `Starts at: U$${priceDefault}`;
+
+        imageDiv.appendChild(eventImage);
+        titleDiv.appendChild(eventTitle);
+        artistsDiv.appendChild(eventArtists);
+        dateDiv.appendChild(eventDate);
+        placeDiv.appendChild(eventPlace);
+        priceDiv.appendChild(eventPriceStart);
+
+        eventDiv.appendChild(imageDiv);
+        eventDiv.appendChild(titleDiv);
+        eventDiv.appendChild(artistsDiv);
+        eventDiv.appendChild(dateDiv);
+        eventDiv.appendChild(placeDiv);
+        eventDiv.appendChild(priceDiv);
 
         EVENTS.appendChild(eventDiv);
     });
